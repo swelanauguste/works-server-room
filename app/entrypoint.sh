17679ad76fe785b2acfabbc27f53705c314f41ef
swelanauguste <swelanauguste@gmail.com>
2024-10-02 07:56:07 -0400

    dockerized

---
+++ app/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
+
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#       sleep 0.1
+#     done
+
+#     echo "PostgreSQL started"
+# fi
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser --noinput
+# python manage.py collectstatic --noinput
+python manage.py racks_stacks
+python manage.py switches
+python manage.py vlans
+python manage.py aruba
+python manage.py cisco
+python manage.py avaya11
+python manage.py avaya12
+python manage.py avaya21
+python manage.py avaya22
+
+exec "$@"
